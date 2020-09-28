42e1067e37ca46a6a90684d072ef48f9495506fd
josuechevez <67529112+josuechevez@users.noreply.github.com>
2020-09-27 19:47:24 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    Swal.fire({
+        title: '',
+        text: '',
+        imageUrl: 'img/119735564_1528242624045448_2879411340304909781_o.png',
+        imageWidth: 600,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+    })
+    
+    
+
+});
